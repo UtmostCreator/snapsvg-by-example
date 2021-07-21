d8e7107242e9d4c2dbcd6884211a9323502e2314
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 15:54:18 +0300

    fix: full circle is shown

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,4 @@ console.log('index is ready');
 // NOTE: windos.onload is required if you put the snapsvg in the header;
 // base snapsvg obj
 let s = Snap(200, 200);
-let circle = s.circle(0, 0, 100); // centerOfTheCircle - x, y, radius
+let circle = s.circle(100, 100, 100); // centerOfTheCircle - x, y, radius
