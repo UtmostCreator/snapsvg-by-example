45cacfd85e194f9f4b7f4bb4dcacae65d5656a4b
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 16:01:50 +0300

    feat: paper - area increased; rect obj added as a square

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ console.log('index is ready');
 
 // NOTE: windos.onload is required if you put the snapsvg in the header;
 // base snapsvg obj
-let s = Snap(200, 200);
+let s = Snap(400, 200); // paper - working area
 let circle = s.circle(100, 100, 100); // centerOfTheCircle - x, y, radius
+let rect = s.rect(200, 0, 200, 200); // x, y point to the top left corner
