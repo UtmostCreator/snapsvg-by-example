49be091777d184487b17cbc95944ad83e22dc1ce
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 15:49:53 +0300

    feat: first example of circle object)

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 console.log('index is ready');
 
+// NOTE: windos.onload is required if you put the snapsvg in the header;
 // base snapsvg obj
-let s = Snap();
+let s = Snap('#svg');
+let circle = s.circle(150, 150, 100);
