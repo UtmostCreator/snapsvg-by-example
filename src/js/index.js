9fa04b41ae83cf6c108e8cca2d89a568280ee2ea
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 16:22:56 +0300

    feat: complex shape polyline added

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,9 @@ let s = Snap(400, 400); // paper - working area
 // let ellipse = s.ellipse(200, 200, 100, 200); // x, y, w, h
 // let roundRect = s.rect(0, 0, 400, 400, 10, 50); // xPos, yPos, h, w, angleOneRoundness, andgeTwoRoundness
 // let roundRect = s.rect(0, 0, 400, 400, 50); // xPos, yPos, h, w, angleOneRoundness, andgeTwoRoundness
-let line = s.line(0, 0, 400, 400);
-line.attr({
-    stroke: '#F00'
-});
+// let line = s.line(0, 0, 400, 400);
+// line.attr({
+//     stroke: '#F00'
+// });
+
+let polygon = s.polyline(0, 0, 50, 50, 100, 0, 150, 50, 150, 150, 100, 100, 50, 150, 0, 100);
