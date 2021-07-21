c5b0983dfd2022e0b0ffbff6b71225eed8181c99
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 16:27:18 +0300

    feat: get back to circle; chaning a shape color and the stroke

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,11 @@ console.log('index is ready');
 // NOTE: windos.onload is required if you put the snapsvg in the header;
 // base snapsvg obj
 let s = Snap(400, 400); // paper - working area
-// let circle = s.circle(100, 100, 100); // centerOfTheCircle - x, y, radius
+let circle = s.circle(200, 200, 200); // centerOfTheCircle - x, y, radius
+circle.attr({
+    fill: '#00ff00',
+    stroke: '#000',
+});
 // let rect = s.rect(200, 0, 200, 200); // x, y point to the top left corner
 
 
@@ -15,4 +19,4 @@ let s = Snap(400, 400); // paper - working area
 //     stroke: '#F00'
 // });
 
-let polygon = s.polyline(0, 0, 50, 50, 100, 0, 150, 50, 150, 150, 100, 100, 50, 150, 0, 100);
+// let polygon = s.polyline(0, 0, 50, 50, 100, 0, 150, 50, 150, 150, 100, 100, 50, 150, 0, 100);
