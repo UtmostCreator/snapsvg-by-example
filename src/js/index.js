9f0be49068aff9b5632ac2a1b5db243e2e746d77
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 16:09:07 +0300

    feat: flatter shape of ellipse

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,9 @@ console.log('index is ready');
 
 // NOTE: windos.onload is required if you put the snapsvg in the header;
 // base snapsvg obj
-let s = Snap(400, 200); // paper - working area
+let s = Snap(400, 400); // paper - working area
 // let circle = s.circle(100, 100, 100); // centerOfTheCircle - x, y, radius
 // let rect = s.rect(200, 0, 200, 200); // x, y point to the top left corner
 
 
-let ellipse = s.ellipse(200, 200, 200, 200); // x, y, w, h
+let ellipse = s.ellipse(200, 200, 200, 100); // x, y, w, h
