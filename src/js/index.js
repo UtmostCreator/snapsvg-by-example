a6c5afb16ee75fdca4ff17799c058a3cb4c9d46d
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 16:37:12 +0300

    feat: building a house; base and roof added as a separate shapes

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,8 @@ console.log('index is ready');
 // NOTE: windos.onload is required if you put the snapsvg in the header;
 // base snapsvg obj
 let s = Snap(400, 400); // paper - working area
+let base = s.rect(100, 200, 200, 200);
+let roof = s.polygon(75, 200, 325, 200, 200, 50);
+base.attr({
+    fill: 'red',
+});
