bff90c817183b685b36f79f7b35ed78bf14b8a8c
UtmostCreator <romazahrypa@gmail.com>
2021-07-21 16:39:32 +0300

    feat: groupping object into 1 to style them together

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,8 @@ console.log('index is ready');
 let s = Snap(400, 400); // paper - working area
 let base = s.rect(100, 200, 200, 200);
 let roof = s.polygon(75, 200, 325, 200, 200, 50);
-base.attr({
-    fill: 'red',
-});
+let house = s.group(base, roof);
 
-roof.attr({
+house.attr({
     fill: 'red',
 });
